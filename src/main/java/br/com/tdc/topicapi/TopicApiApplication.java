58f844c7a62f51c05b1dea15e11e2e784056d53a
Francisco Teixeira <franciscosft@gmail.com>
2021-08-17 15:56:25 -0300

    Criando estrutura inicial do projeto

---
+++ src/main/java/br/com/tdc/topicapi/TopicApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.tdc.topicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tdc/topicapi/TopicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tdc.topicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
